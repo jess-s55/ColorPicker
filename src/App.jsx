449ba07e996aae4c2063ff99e3881bca19226ca0
Jessica Spivak <jls103193@yahoo.com>
2023-07-02 10:00:11 -0400

    tab 3, Picking Colors is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
+import { useState } from 'react';
+
 // Write your Color component here
-const Color = (props) => {
-  return <div className={props.color}></div>
+const Color = ({color, setSelectedColor}) => {
+  return (
+    <div 
+      className={color} 
+      onClick={() => setSelectedColor(color)} 
+     ></div>
+  )
 }
+
+// const Color = (props) => {
+//   return <div className={props.color}></div>
+// }
 
 /* or using object deconstruction */
 // const Color = ({color}) => {
 //   return <div className={color}></div>
 // }
 
+
+
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("")
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color="blue"/>
-        <Color color="red"/>
-        <Color color="green"/>
+        <Color color="blue" setSelectedColor={setSelectedColor}/>
+       <Color color="red" setSelectedColor={setSelectedColor}/>
+        <Color color="green" setSelectedColor={setSelectedColor}/>
       </div>
     </div>
   );
